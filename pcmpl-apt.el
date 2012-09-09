5bd01c0d01ee44bc1cb6314078c52b4239f81221
Damien Cassou <damien.cassou@gmail.com>
2012-09-09 17:38:02 +0200

    Rename apt-get -> apt
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>

--- pcmpl-apt.el
+++ pcmpl-apt.el
@@ -1,31 +1,38 @@
-;;; pcomplete-apt-get.el --- functions for dealing with apt-get completions
-
-;; Copyright (C) 2012 Free Software Foundation, Inc.
-
+;;; pcmpl-apt.el --- functions for dealing with Debian apt completions
+;;
+;; Copyright (C) 2012 Damien Cassou
+;;
 ;; Author: Damien Cassou <damien.cassou@gmail.com>
-;; Package: pcomplete
-
-;; This file is part of GNU Emacs.
-
-;; GNU Emacs is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; GNU Emacs is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
+;; Url: https://github.com/DamienCassou/emacs-pcomplete-plugins
+;; GIT: https://github.com/DamienCassou/emacs-pcomplete-plugins
+;; Version: 0.1
+;; Created: 2012-09-07
+;; Keywords: emacs package eshell pcomplete completion apt apt-get
+;;
+;; This file is NOT part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
-
+;; along with this program ; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+;;
 ;;; Commentary:
-
-;; These functions provide completion rules for the `apt-get' tool.
-
+;;
+;; This code provides eshell completion for apt-get, apt-cache and
+;; aptitude Debian tools.
+;;
 ;;; Code:
-
+
 (require 'pcomplete)
 
 (defgroup pcmpl-apt-get nil
@@ -98,6 +105,6 @@
   (pcomplete-uniqify-list
    (split-string (shell-command-to-string command))))
 
-(provide 'pcomplete-apt-get)
+(provide 'pcmpl-apt)
 
-;;; pcomplete-apt-get.el ends here
+;;; pcmpl-apt.el ends here
